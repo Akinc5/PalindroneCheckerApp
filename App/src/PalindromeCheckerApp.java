2517ac81f380b9c5c8979afffe1b9d546dc63995
Akshansh <akshansh9816264920@gmail.com>
2026-02-23 11:24:14 +0530

    Akshansh Added UC7

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,23 +1,20 @@
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
+import java.util.*;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner hi = new Scanner(System.in);
         System.out.println("Input Text:");
         String input = hi.nextLine();
-        Queue<Character> qt = new LinkedList<>();
-        Stack<Character> st = new Stack<>();
+        Deque<Character> dt = new ArrayDeque<>();
         for (char c : input.toCharArray()) {
-            st.push(c);
-            qt.add(c);
+            dt.add(c);
         }
         boolean isPalindrome = true;
-        while(!qt.isEmpty()){
-            if (!qt.remove().equals(st.pop())) {
+        while(dt.size()>1){
+            if (!dt.removeFirst().equals(dt.removeLast())) {
                 isPalindrome = false;
-                break;}
+                break;
+            }
         }
         System.out.println("Is it a Palindrome? : " + isPalindrome);
         hi.close();
